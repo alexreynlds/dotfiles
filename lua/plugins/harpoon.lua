ed7d390a52fe862c2fc86d94131e096d98414131
alexreynlds <ajrey@live.co.uk>
2024-04-17 22:34:39 +0100

    push

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,6 +4,7 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
+		-- Keybindings
 		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add file to harpoon" })
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
 	end,
