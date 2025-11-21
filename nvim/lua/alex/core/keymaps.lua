8e125658175f82facf408464ccc55535c2ce8480
alexreynolds <ajrey@live.co.uk>
2025-11-21 13:08:22 +0000

    + Updating hypr and neovim

--- nvim/lua/alex/core/keymaps.lua
+++ nvim/lua/alex/core/keymaps.lua
@@ -10,6 +10,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", '"_dP')
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+Y')
 
+-- Make "+Y behave like "+y
+vim.keymap.set("n", '"+Y', '"+y', { noremap = true, silent = true })
+vim.keymap.set("x", '"+Y', '"+y', { noremap = true, silent = true })
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
